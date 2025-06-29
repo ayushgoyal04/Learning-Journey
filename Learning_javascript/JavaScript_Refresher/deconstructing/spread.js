fbdadcf387a1e5a4f4fdbff3ebd9a729ee504c11
ayushgoyal04 <ayushgoyal434@gmail.com>
2025-06-29 17:09:20 +0530

    node

---
+++ Learning_javascript/JavaScript_Refresher/deconstructing/spread.js
@@ -0,0 +1,17 @@
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+
+const c = [...a, ...b, 11, 12, 9]; // ... is the spread operator, it does nto collect the array, but the properties of the array, therefore it will not nest the entire array
+
+// console.log(c) // [1, 2, 3, 4, 5, 6, 11, 12, 9]
+
+
+function f1(a, b, ...rem) {
+    console.log(a)
+    console.log(b)
+
+    console.log(rem)
+
+}
+
+f1(10, 20, 30, 40, 50);
