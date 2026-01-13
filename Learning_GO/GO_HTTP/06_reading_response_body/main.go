c88d77beab9a9007391c3371c6fd61c9fbe52715
ayushgoyal04 <ayushgoyal434@gmail.com>
2026-01-13 17:26:51 +0530

    http checkpoint

---
+++ Learning_GO/GO_HTTP/06_reading_response_body/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	url := "https://jsonplaceholder.typicode.com/todos"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK{
+		fmt.Println(resp.Status)
+		return
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		bodyText := string(bodyBytes)
+
+		max := 250
+		fmt.Println(len(bodyText))
+		if len(bodyText) < max {
+			max = len(bodyText)
+		}
+
+		fmt.Println(bodyText[:max])
+
+}
