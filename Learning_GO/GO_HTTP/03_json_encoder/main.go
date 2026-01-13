aadb92c7ebc66e45e0476188000de18d381a16bb
ayushgoyal04 <ayushgoyal434@gmail.com>
2026-01-13 15:36:59 +0530

    json encoding

--- Learning_GO/GO_HTTP/03_json_encoder/main.go
+++ Learning_GO/GO_HTTP/03_json_encoder/main.go
@@ -1,2 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"encoding/json"
+)
+
+func successHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+
+	res := map[string]any{
+		"ok" : true,
+		"message" : "Json encoded successful",
+		"datetime": time.Now().UTC(),
+	}
+
+	_ = json.NewEncoder(w).Encode(res)
+}
+
+func main() {
+
+	http.HandleFunc("/ok", successHandler)
+
+	err := http.ListenAndServe(":5000", nil)
+	fmt.Println(err)
+}
